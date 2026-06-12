4bff6d36aebeb6e06c09185e6bcfb61c7beaa260
CarlosJX7 <carlos.jx7@gmail.com>
2026-06-12 23:49:30 +0200

    Add disorder dir

---
+++ srcs/disorder.c
@@ -0,0 +1,18 @@
+#include "../includes/push_swap.h"
+
+/*
+function compute_disorder(stack a):
+mistakes = 0
+total_pairs = 0
+for i from 0 to size(a)-1:
+for j from i+1 to size(a)-1:
+total_pairs += 1
+if a[i] > a[j]:
+mistakes += 1
+return mistakes / total_pairs
+*/
+
+int	compute_disorder(t_stack *stack)
+{
+	
+}
